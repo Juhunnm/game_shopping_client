88b014d021eeb1da099c18a3218a884226ddd472
juhun <minjuhun1@gmail.com>
2024-04-24 12:56:24 +0900

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+const App = () => {
+    const [data,setData]=useState("");
 
-function App() {
+    useEffect(()=>{
+        axios.get("http://localhost:8080/api/data")
+        .then((res)=>{
+            setData(res.data);
+        })
+        .catch((err)=>{
+            console.log("ERR data : ",err);
+        })
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Respoense from spring boot : </h1>
+      {data}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
